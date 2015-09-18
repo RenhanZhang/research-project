8cae299b435af13ee2fc77b851a8e3d881d77243
Renhan Zhang <rhzhang@umich.edu>
2015-09-18 12:52:44 -0400

    add functions to see # of words distribution

---
+++ files/test.py
@@ -0,0 +1,37 @@
+import MySQLdb
+from bs4 import BeautifulSoup
+import gc
+conn = MySQLdb.connect(host='lit04.eecs.umich.edu',
+                       user='rhzhang',
+                       passwd='johnjohn',
+                       db='myblogs',
+                       charset='utf8')
+
+# ipdb.set_trace()
+cur = conn.cursor()
+
+cmd = 'select pf.url as profile_url, po.content \
+       from profiles as pf, profiles_blogs as p_b, blogs_posts as b_p, posts as po \
+       where \
+       pf.url = p_b.profile_url and p_b.blog_url = b_p.blog_url and b_p.post_url = po.url;'
+
+cur.execute(cmd)
+
+dic = {}
+count = 0
+for e in cur.fetchall():
+    count += 1
+    raw_text = e[1].encode('utf-8')
+    soup = BeautifulSoup(raw_text)
+    text = soup.get_text()
+    #print '--------------------------------------------------'
+    #print text
+    dic[e[0]] = dic.get(e[0], 0) + len(text.split())
+
+    if count > 10000:
+      count = 0
+      gc.collect()
+
+with open('result_partial.csv', 'a') as f:
+    for p in dic:
+        f.write(p + ', ' + str(dic[p]) + '\n')
