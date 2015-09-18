081d5a709027601365cbcfb3e2c49afd4f1773cd
Renhan Zhang <rhzhang@umich.edu>
2015-09-18 19:22:39 -0400

    modified test to view 500000

--- files/test.py
+++ files/test.py
@@ -1,11 +1,13 @@
 import MySQLdb
+import MySQLdb.cursors
 from bs4 import BeautifulSoup
 import gc
 conn = MySQLdb.connect(host='lit04.eecs.umich.edu',
                        user='rhzhang',
                        passwd='johnjohn',
                        db='myblogs',
-                       charset='utf8')
+                       charset='utf8',
+                       cursorclass = MySQLdb.cursors.SSCursor)
 
 # ipdb.set_trace()
 cur = conn.cursor()
@@ -13,13 +15,13 @@ cur = conn.cursor()
 cmd = 'select pf.url as profile_url, po.content \
        from profiles as pf, profiles_blogs as p_b, blogs_posts as b_p, posts as po \
        where \
-       pf.url = p_b.profile_url and p_b.blog_url = b_p.blog_url and b_p.post_url = po.url;'
+       pf.url = p_b.profile_url and p_b.blog_url = b_p.blog_url and b_p.post_url = po.url limit 500000;'
 
 cur.execute(cmd)
 
 dic = {}
 count = 0
-for e in cur.fetchall():
+for e in cur:
     count += 1
     raw_text = e[1].encode('utf-8')
     soup = BeautifulSoup(raw_text)
@@ -27,10 +29,9 @@ for e in cur.fetchall():
     #print '--------------------------------------------------'
     #print text
     dic[e[0]] = dic.get(e[0], 0) + len(text.split())
+    del e
+    print count
 
-    if count > 10000:
-      count = 0
-      gc.collect()
 
 with open('result_partial.csv', 'a') as f:
     for p in dic:
